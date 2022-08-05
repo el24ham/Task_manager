ee9585c921d3e22d6ddbfff640d8e60ad5116715
Fazel <fazel.haghighi1399@gmail.com>
2022-08-06 01:26:37 +0430

    just changed date inout

--- Task Manager/main.py
+++ Task Manager/main.py
@@ -1,10 +1,11 @@
+from posixpath import split
 import database as db
 import plot as p
 import os
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
-    
+           
 def hint():
     print("Available Commands: ")
     print("""
@@ -38,9 +39,37 @@ while True:
         print("Task: ")
         task = input()
         print("Start Time:(Standard format: Year/Month/Day) ")
-        start = input()
+        first = input()
+        
+        start = ""
+        start += str(int(first.split("/")[0]) - 2000) + "/" 
+        
+        if int(first.split("/")[1]) < 10:
+            start += "0" + str(int(first.split("/")[1])) + "/"
+        else:
+            start += str(int(first.split("/")[1])) + "/"
+            
+        if int(first.split("/")[2]) < 10:
+            start += "0" + str(int(first.split("/")[2]))
+        else:
+            start += str(int(first.split("/")[2]))
+            
         print("Finish Time:(Standard format: Year/Month/Day) ")
-        finish = input()
+        first = input()
+        
+        finish = ""
+        finish += str(int(first.split("/")[0]) - 2000) + "/" 
+        
+        if int(first.split("/")[1]) < 10:
+            finish += "0" + str(int(first.split("/")[1])) + "/"
+        else:
+            finish += str(int(first.split("/")[1])) + "/"
+            
+        if int(first.split("/")[2]) < 10:
+            finish += "0" + str(int(first.split("/")[2]))
+        else:
+            finish += str(int(first.split("/")[2]))
+        
         db.insert_task(person, start, finish, task)
         
         clear()
