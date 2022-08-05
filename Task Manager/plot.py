e0d88b809369593f8681819cc7bf87d122ac78a9
Elham <elham.ghaemi224@gmail.com>
2022-08-05 22:39:18 +0430

    new

--- Task Manager/plot.py
+++ Task Manager/plot.py
@@ -4,9 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from tabledef import Task
 from tabledef import *
 
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import numpy as np
+#import matplotlib.pyplot as plt; plt.rcdefaults()
+#import numpy as np
 import matplotlib.pyplot as plt
+#import pandas as pd
 
 engine = create_engine('sqlite:///tasks.db', echo=True)
 
@@ -17,17 +18,32 @@ def plot_show1(person):
     dict={}
     for task in session.query(Task).filter(Task.name == person):
         list=[]
-        #s=task.start + "-" + task.finish
+        
         time1=task.start
         time1=datetime.strptime(time1,'%y/%m/%d')
         list.append(time1)
         time2=task.start
         time2=datetime.strptime(time2,'%y/%m/%d')
         list.append(time2)
-        time_interval = time2 - time1
-        list.append(time_interval)
         list.append(task.task)
         dict[task.id] = list
-    #sort_orders=sorted    
+    
+    sort_orders=sorted(dict.items[1](),key=lambda x : x[1])
+    x=[]
+    y=[]
+    lables=[]
+    for i in sort_orders:
+        s=i[0] + "-" + i[1]
+        x.append(s)
+        time_interval = i[1] - i[0]
+        y.append(time_interval)
+        lables.append(i[2])
+        
+    plt.bar(x,y,fc="lightgray",ec="black")
+    for i in range(len(x)):
+        plt.text(i,y[i],lables[i],ha="center",va="bottom")
+    
+    plt.show()            
+                           
         
     
