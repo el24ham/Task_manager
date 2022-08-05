1d8a6b944af8144e633e6f2c0d2c074ff72f55f6
Fazel <fazel.haghighi1399@gmail.com>
2022-08-05 23:53:16 +0430

    fixed

--- Task Manager/main.py
+++ Task Manager/main.py
@@ -96,7 +96,9 @@ while True:
     elif msg=="plot" :
         
         clear()
-        p.show_plot()
+        print("Enter name: ")
+        person = input()
+        p.plot_show1(person)
              
     else:
         clear()
