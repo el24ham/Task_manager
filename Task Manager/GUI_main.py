cdc53cb2097ad5855348ae4277f4da3b7fa11486
Fazel <fazel.haghighi1399@gmail.com>
2022-08-07 11:42:03 +0430

    shit

--- Task Manager/GUI_main.py
+++ Task Manager/GUI_main.py
@@ -1,11 +1,29 @@
+from turtle import color, title
+from unicodedata import name
 import PySimpleGUI as sg
+from tkinter import *
 
-layout = [[sg.Text("Hello there, welcome to our app")], [sg.Text("Select the option you want")],[sg.Button("add")], [sg.Button("remove")], [sg.Button("person task")], [sg.Button("tasks")], [sg.Button("plot")], [sg.Button("exit")]]
+layout = [[sg.Text("Hello there, welcome to our app")], [sg.Text("Select the option you want:")],[sg.Button("add")], [sg.Button("remove")], [sg.Button("person task")], [sg.Button("tasks")], [sg.Button("plot")], [sg.Button("exit")]]
 window = sg.Window("Task Manager", layout, margins=(300, 150))
 
 while True:
     event, values = window.read()
     if event == "exit" or event == sg.WIN_CLOSED:
         break
+    elif event == "add": 
+        add = Tk()
+        add.geometry("600x300")
+        add.title("Adding")
+  
+        label = Label(add,
+                text ="Name:",
+                foreground="blue",
+                background="green")
+        entry = Entry(add)
 
+        label.pack(fill=BOTH, expand=True)
+        entry.pack()
+
+    mainloop()
+          
 window.close()
